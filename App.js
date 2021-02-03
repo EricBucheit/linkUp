68656d943973a88e5ea66f5f2f205a1e3df43f84
ERICASTPOS <ericb.astpos@gmail.com>
2021-02-03 15:40:21 -0800

    clean refresh on add

--- App.js
+++ App.js
@@ -12,65 +12,49 @@ const DATA = [
   {
     id: "1",
     title: "First Item",
+    links: [
+      {
+        id: "11",
+        name: "Google",
+        link: "http://google.com",
+      },
+      {
+        id: "12",
+        name: "Ericb",
+        link: "http://ericbucheit.com",
+      },
+      {
+        id: "13",
+        name: "REDDIT",
+        link: "http://reddit.com",
+      },
+      {
+        id: "14",
+        name: "Google",
+        link: "http://google.com",
+      },
+      {
+        id: "15",
+        name: "Google",
+        link: "http://google.com",
+      },
+    ],
   },
 ];
 
 
-const LinkData = [
-  {
-    id: "11",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "12",
-    name: "Ericb",
-    link: "http://ericbucheit.com",
-  },
-  {
-    id: "13",
-    name: "REDDIT",
-    link: "http://reddit.com",
-  },
-  {
-    id: "14",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "15",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "16",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "17",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "18",
-    name: "Google",
-    link: "http://google.com",
-  },
-  {
-    id: "19",
-    name: "Google",
-    link: "http://google.com",
-  },
-];
+const Home = ({currentCategory, setCurrentCategory, data, setData}) => <LinkCategories setData={setData} data={data} setCurrentCategory={setCurrentCategory} />;
+
+const LinkPage = ({currentCategory, setCurrentCategory, data, setData}) => <Links data={data} setData={setData} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}/>;
+
+const App = () => {
+
+let [currentCategory, setCurrentCategory] = React.useState({})
+let [data, setData] = React.useState([]);
 
 
 
-const Home = () => <LinkCategories />;
-
-const About = () => <Links />;
-
-const App = () => (
+return (
   <NativeRouter>
     <View style={styles.container}>
       <View style={styles.nav}>
@@ -85,26 +69,24 @@ const App = () => (
           <Text>Links</Text>
         </Link>
       </View>
-      <Route exact path="/" component={Home} />
-      <Route path="/link" component={About} />
+      <Route exact path="/" component={() => <Home data={data} setData={setData} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory} />} />
+      <Route path="/link" component={() => <LinkPage data={data} setData={setData} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory} />} />
 
     </View>
-    <AddCategoryModal />
-
   </NativeRouter>
-);
+
+  )
+};
 
 
 const LinkItem = ({ item, onPress, style }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
     <Button title={item.name} onPress={ ()=>{ Linking.openURL(item.link)}} />
-
   </TouchableOpacity>
 );
 
-const Links = () => {
+const Links = ({currentCategory, setData, data}) => {
   const [selectedId, setSelectedId] = useState(null);
-
   const renderItem = ({ item }) => {
     const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
     return (
@@ -123,66 +105,19 @@ const Links = () => {
     <SafeAreaView style={styles.container}>
     <Text> Links </Text>
       <FlatList
-        data={LinkData}
+        data={currentCategory.links}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         extraData={selectedId}
       />
+      <AddLinkModal setData={setData} data={data} currentCategory={currentCategory}/>
     </SafeAreaView>
   );
 };
 
-
-
-
-const Item = ({ item, onPress, style }) => (
-  
-    <Link
-      to="/link"
-      underlayColor="#f0f4f7"
-      style={[styles.item, style]}
-    >
-      <Text style={styles.title}>{item.title}</Text>
-    </Link>
-
-
-);
-
-const LinkCategories = () => {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-
-    return (
-      <Item
-        item={item}
-        onPress={() => {
-          setSelectedId(item.id)
-
-        }}
-        style={{ backgroundColor }}
-      />
-    );
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-      />
-    </SafeAreaView>
-  );
-};
-
-
-
-
-const AddCategoryModal = () => {
+const AddLinkModal = ({currentCategory, setData, data}) => {
   const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -192,10 +127,11 @@ const AddCategoryModal = () => {
         onRequestClose={() => {
           Alert.alert("Modal has been closed.");
         }}
+        currentCategory={currentCategory}
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-           <Input setModalVisible={setModalVisible}/>
+           <AddLinkInput setData={setData} data={data} currentCategory={currentCategory} setModalVisible={setModalVisible}/>
               <View style={styles.modalButtonWrapper}>
             <TouchableHighlight
               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
@@ -220,9 +156,131 @@ const AddCategoryModal = () => {
 };
 
 
-const Input = ({setModalVisible}) => {
-  const [value, onChangeText] = React.useState('');
+const AddLinkInput = ({setModalVisible, currentCategory, setData, data}) => {
+  const [url, setUrl] = React.useState('');
+  const [name, setName] = React.useState('');
+  return (
+    <View>
+    <Text>Name</Text>
+    <TextInput
+      style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setName(text)}
+      value={name}
+    />
+    <Text>URL</Text>
+    <TextInput
+      style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setUrl(text)}
+      value={url}
+    />
 
+    <TouchableHighlight
+      style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+      onPress={() => {
+        //needed
+        // parse url for https://
+        currentCategory.links.push({
+          id: `${currentCategory.links.length + 1}`,
+          name: name,
+          link: url,
+        })
+        data = data.slice();
+        setData(data);
+
+        if (setModalVisible) {
+          setModalVisible(false);
+        }
+      }}
+    >
+      <Text style={styles.textStyle}>Add</Text>
+    </TouchableHighlight>
+    </View>
+  );
+}
+
+const Item = ({ item, onPress, style }) => (
+  
+    <Link
+      to="/link"
+      underlayColor="#f0f4f7"
+      style={[styles.item, style]}
+      onPress={onPress}
+    >
+      <Text style={styles.title}>{item.title}</Text>
+    </Link>
+
+
+);
+
+const LinkCategories = ({setCurrentCategory, data, setData}) => {
+  const [selectedId, setSelectedId] = useState(null);
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    return (
+      <Item
+        item={item}
+        onPress={() => {
+          setCurrentCategory(item)
+        }}
+        style={{ backgroundColor }}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+      />
+      <AddCategoryModal setData={setData} data={data} setData={setData}/>
+    </SafeAreaView>
+  );
+};
+
+const AddCategoryModal = ({setData, data}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+           <Input data={data} setData={setData} setModalVisible={setModalVisible}/>
+              <View style={styles.modalButtonWrapper}>
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+
+            </View>
+          </View>
+        </View>
+      </Modal>
+        <Icon name='add-circle-outline' 
+          onPress={() => {
+            setModalVisible(true);
+          }} size={40} />
+      
+    </View>
+  );
+};
+
+
+const Input = ({setModalVisible, data, setData}) => {
+  const [value, onChangeText] = React.useState('');
   return (
     <View>
     <TextInput
@@ -234,11 +292,17 @@ const Input = ({setModalVisible}) => {
     <TouchableHighlight
       style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
       onPress={() => {
-        DATA.push({
-          id: `${DATA.length + 1}`,
+        
+        data.push({
+          id: `${data.length + 1}`,
           title: value,
+          links: [],
         })
-        setModalVisible(false);
+        data = data.slice();
+        setData(data);
+        if (setModalVisible) {
+          setModalVisible(false);
+        }
       }}
     >
       <Text style={styles.textStyle}>Add</Text>
